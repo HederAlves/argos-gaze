665d354a8e344e8b39064a78ff30f96e31f6ab3b
hederalves <alvesheder92@gmail.com>
2024-06-14 04:53:40 -0300

    feat: create component campaign list

---
+++ src/components/campaign-list/CampaignList.tsx
@@ -0,0 +1,69 @@
+import React from 'react';
+import styled from 'styled-components';
+import { AdCampaign } from '../Ad-campaigns/AdCampaigns';
+
+interface AdCampaignListProps {
+    campaigns: AdCampaign[];
+}
+
+const AdCampaignListContainer = styled.div`
+    display: flex;
+    flex-direction: column;
+    max-height: 502px;
+    margin: '15px';
+    overflow-y: auto;
+    overflow-x: hidden;
+    scrollbar-width: thin;
+    scrollbar-color: #ccc #f0f0f0; 
+    gap: 20px;
+    padding: 20px;
+    heigth: 50px
+`;
+
+const AdCampaignCard = styled.div`
+    background-color: #fff;
+    border: 1px solid #ddd;
+    border-radius: 8px;
+    box-shadow: 0 2px 4px rgba(0, 0, 0, 0.1);
+    width: calc(33.333% - 40px);
+    box-sizing: border-box;
+    display: flex;
+    flex-direction: column;
+    gap: 10px;
+    width: 240px;
+
+    h2 {
+        margin-top: 0;
+        font-size: 1.5em;
+    }
+
+    p {
+        margin: 0;
+    }
+
+    strong {
+        font-weight: bold;
+    }
+`;
+
+const CampaignList: React.FC<AdCampaignListProps> = ({ campaigns }) => {
+    return (
+        <AdCampaignListContainer>
+            {campaigns.map(campaign => (
+                <AdCampaignCard key={campaign.id}>
+                    <div style={{ background: '#f8f9fa', textAlign: 'center', paddingTop: '10px' }}>
+                        <h2>{campaign.outdoors}</h2>
+                    </div>
+                    <div style={{ padding: '0px 16px 16px', height: '140px', display: 'flex', flexDirection: 'column', justifyContent: 'center', gap: '12px' }}>
+                        <p><strong>Campanha:</strong> {campaign.name}</p>
+                        <p><strong>Empresa Responsável:</strong> {campaign.responsible_company}</p>
+                        <p><strong>Cidade:</strong> {campaign.city}</p>
+                        <p><strong>Endereço:</strong> {campaign.address}</p>
+                    </div>
+                </AdCampaignCard>
+            ))}
+        </AdCampaignListContainer>
+    );
+};
+
+export default CampaignList;
